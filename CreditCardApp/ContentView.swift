016e5cb493afb4fc57db0a9b14ddffaf0c0984ed
Wajid Patan <wajid@live.ca>
2024-11-20 16:24:31 -0700

    Initial Commit

---
+++ CreditCardApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreditCardApp
+//
+//  Created by Wajid Patan on 11/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreditCardApp/CreditCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardAppApp.swift
+//  CreditCardApp
+//
+//  Created by Wajid Patan on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
